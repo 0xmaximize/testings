4dceb07bfac0575249b9e635a43301eaf2199fbe
0xmaximize <135501343+0xmaximize@users.noreply.github.com>
2023-06-05 16:19:30 +0700

    Test

---
+++ pages/Origin.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Origin: NextPage = () => {
+  return (
+    <div className={styles.container}>
+   
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+           Origin NFT
+        </h1>
+        <p className={styles.description}>
+         Build decentralization & Web3.0 Revolutionary with new experience
+        </p>
+
+        <div className={styles.connect}>
+        
+        </div>
+
+        
+      </main>
+    </div>
+  );
+};
+
+export default Origin;
