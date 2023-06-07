fbf61bed57a231271bb5da47d08e7000fc66e339
0xmaximize <135501343+0xmaximize@users.noreply.github.com>
2023-06-07 20:42:38 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,17 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import Header from "../components/Header";
 import { NextUIProvider, createTheme } from '@nextui-org/react';
 import "../styles/globals.css";
-import { Helmet } from "react-helmet" ;
-
-const TITLE = 'Arbi Network | Web3.0 & DEX on Arbitrum';
+import { Helmet } from "react-helmet";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const activeChain = "arbitrum";
-
 const darkTheme = createTheme({
 	type: 'light',  
 });
+const TITLE = 'Arbi Network | Web3.0 & DEX on Arbitrum';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
